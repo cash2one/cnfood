2ed64ad423058ca3b34724905acfd54206f754a6
weiguagnchen <614850049@qq.com>
2017-06-18 11:56:41 +0800

    还差详情页

---
+++ build/js/channel.js
@@ -0,0 +1,18 @@
+'use strict';
+
+$(function () {
+    var mySwiper = $('.swiper-container').swiper({
+        autoplay: 3000,
+        loop: true,
+        pagination: '.pagination',
+        autoplayDisableOnInteraction: false
+    });
+    var leftArrow = $('.channel-focus .icon-arrow-left-copy');
+    var rightArrow = $('.channel-focus .icon-arrow-right');
+    leftArrow.click(function () {
+        mySwiper.swipePrev();
+    });
+    rightArrow.click(function () {
+        mySwiper.swipeNext();
+    });
+});
